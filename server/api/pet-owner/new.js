29195f631be0ee9f2574f39ee04ddc48ed0cefbc
nurinaamira <43457350+nurinaamira@users.noreply.github.com>
2023-05-18 00:01:55 +0800

    api pet

---
+++ server/api/pet-owner/new.js
@@ -0,0 +1,66 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export default defineEventHandler(async (event) => {
+  // bookName, bookSynopsis, bookAuthor
+
+  try {
+    // New way
+    const {
+      petName: nama,
+      petType: type,
+      petBreed: breed,
+      petDescription: description,
+      vaccinationStatus: vaccstatus,
+      dewormStatus: deworm,
+      petGender: gender,
+      petLocation: location,
+      petHealth: health,
+      petStatus: petstatus,
+      petFee: fee,
+      petColor: color,
+    } = await readBody(event)
+
+    console.log("aum", readBody(event))
+
+    console.log("nama : " , nama)
+
+    const insertPet = await prisma.pet.create({
+      data: {
+        petName: nama,
+      petType: type,
+      petBreed: breed,
+      petDescription: description,
+      vaccinationStatus: vaccstatus,
+      dewormStatus: deworm,
+      petGender: gender,
+      petLocation: location,
+      petHealth: health,
+      petStatus: petstatus,
+      petFee: fee,
+      petColor: color,
+      },
+    })
+    console.log(insertPet)
+
+    if (!insertPet) {
+      return {
+        statusCode: 500,
+        message: "Gagal menyimpan data",
+        insertPet,
+      }
+    }
+
+    return {
+      statusCode: 200,
+      message: "Berhasil menyimpan data",
+    }
+  } catch (error) {
+    console.log(error)
+    return {
+      statusCode: 500,
+      message: "Masalah pada server",
+    }
+  }
+})

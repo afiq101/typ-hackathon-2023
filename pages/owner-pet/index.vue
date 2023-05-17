9b12556d97beb3ae0788dee6fde0c769f3e7f22c
nurinaamira <43457350+nurinaamira@users.noreply.github.com>
2023-05-17 16:09:49 +0800

    logo and pet form

--- pages/owner-pet/index.vue
+++ pages/owner-pet/index.vue
@@ -1,16 +1,16 @@
 <script setup>
 definePageMeta({
   title: "Manage Pet Owner",
-});
+})
 
-const showModalTop = ref(false);
+const showModalTop = ref(false)
 
 const form = ref({
   customerName: "",
   customerAddress: "",
   customerPhone: "",
   bookFeatured: false,
-});
+})
 
 const data = [
   {
@@ -20,37 +20,51 @@ const data = [
     description: "Manja",
     status: "Active",
     action: "Edit",
-  }
-];
-
+  },
+]
 </script>
 <template>
   <div>
     <LayoutsBreadcrumb />
 
     <rs-card>
-
       <!-- <template #header>
         <h1 class="text-2xl font-semibold">Place</h1>
       </template> -->
 
       <template #body>
-
         <rs-modal title="This is a modal" position="top" v-model="showModalTop">
-          <FormKit type="form" :actions="false" @submit="hantor" :incomplete-message="false">
-            <FormKit v-model="form.customerName" type="text" label="Pet Name" validation="required"
-              validation-visibility="dirty" :validation-messages="{
+          <FormKit
+            type="form"
+            :actions="false"
+            @submit="hantor"
+            :incomplete-message="false"
+          >
+            <FormKit
+              v-model="form.customerName"
+              type="text"
+              label="Pet Name"
+              validation="required"
+              validation-visibility="dirty"
+              :validation-messages="{
                 required: 'Pet name cannot be empty',
-              }">
+              }"
+            >
               <template #label>
                 Pet Name<span class="text-red-500"> *</span>
               </template>
             </FormKit>
 
-            <FormKit v-model="form.customerName" type="text" label="Pet Color" validation="required"
-              validation-visibility="dirty" :validation-messages="{
+            <FormKit
+              v-model="form.customerName"
+              type="text"
+              label="Pet Color"
+              validation="required"
+              validation-visibility="dirty"
+              :validation-messages="{
                 required: 'Pet color cannot be empty',
-              }">
+              }"
+            >
               <template #label>
                 Color<span class="text-red-500"> *</span>
               </template>
@@ -60,48 +74,46 @@ const data = [
           </FormKit>
         </rs-modal>
 
-
-        <br>
+        <br />
         <div class="flex">
           <div class="contents">
             <div class="flex-1">
-                <rs-button variant="primary" class="float-right" @click="showModalTop = true">
+              <nuxt-link to="/owner-pet/newpet">
+                <rs-button variant="primary" class="float-right">
                   <Icon name="material-symbols:add"></Icon>
                   Add
                 </rs-button>
-
+              </nuxt-link>
             </div>
           </div>
         </div>
         <!-- please create datatable of place using rs-table -->
-        <rs-table :data="data" :options-advanced="{
-          sortable: true,
-          responsive: true,
-        }" advanced>
-
+        <rs-table
+          :data="data"
+          :options-advanced="{
+            sortable: true,
+            responsive: true,
+          }"
+          advanced
+        >
           <template v-slot:status="data">
             <div>
               <rs-badge variant="success">Active</rs-badge>
             </div>
           </template>
 
-
           <template v-slot:action="data">
             <div class="flex justify-center">
-
-              <rs-button variant="primary" class="float-right" @click="showModalTop = true">Edit</rs-button>
+              <rs-button
+                variant="primary"
+                class="float-right"
+                @click="showModalTop = true"
+                >Edit</rs-button
+              >
             </div>
           </template>
         </rs-table>
-
-
       </template>
-
-
     </rs-card>
-
-
-
   </div>
 </template>
-      

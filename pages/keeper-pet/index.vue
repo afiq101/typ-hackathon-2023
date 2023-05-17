6aa1105117918e2ecd1f0cdfb8a90381ce2636ce
hishamudin98 <hishamudin.ali@gmail.com>
2023-05-17 21:24:07 +0800

    landing page

--- pages/keeper-pet/index.vue
+++ pages/keeper-pet/index.vue
@@ -8,7 +8,9 @@ const showModalTop = ref(false);
 
 const form = ref({
   ownerName: "",
+  ownerPassword: "",
   ownerUsername: "",
+  ownerEmail: "",
   ownerAddress: "",
   ownerPhone: "",
 });
@@ -29,92 +31,176 @@ const petData = ref([
 // });
 
 // console.log(bookData);
+
+const submitData = async () => {
+
+  if (form.value.ownerName === "" || form.value.ownerUsername === "" || form.value.ownerEmail === "" || form.value.ownerAddress === "" || form.value.ownerPhone === "") {
+    // $toast.error("Please fill in the book name");
+    return;
+  }
+
+  try {
+    const { data } = await useFetch("/api/keeper/add", {
+      method: "POST",
+      body: {
+        ownerName: form.value.ownerName,
+        ownerPassword: form.value.ownerPassword,
+        ownerUsername: form.value.ownerUsername,
+        ownerEmail: form.value.ownerEmail,
+        ownerAddress: form.value.ownerAddress,
+        ownerPhone: form.value.ownerPhone,
+      }
+    });
+
+    console.log(data);
+
+    if (data.value.statusCode == 200) {
+      showModalTop.value = false;
+      alert("Register Success");
+      // $toast.success(data.message);
+    } else {
+      // $toast.error(data.message);
+      alert(data.value.message);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
 </script>
 <template>
-  <rs-modal title="This is a modal" position="top" v-model="showModalTop" :ok-callback="clickOK" >
-    <FormKit type="form" :actions="false" @submit="hantor" :incomplete-message="false">
-      <FormKit v-model="form.customerName" type="text" label="Customer Name" validation="required"
+  <rs-modal title="Register" position="top" v-model="showModalTop">
+    <FormKit type="form" :actions="false" @submit="submitData" :incomplete-message="false">
+      <FormKit v-model="form.ownerName" type="text" label="Customer Name" validation="required"
         validation-visibility="dirty" :validation-messages="{
           required: 'Customer name cannot be empty',
         }">
-        <template #label>
-          Name<span class="text-red-500">*</span>
-        </template>
       </FormKit>
-      <FormKit v-model="form.customerPhone" type="text" label="Phone Number" validation="required"
-        validation-visibility="dirty" :validation-messages="{
-          required: 'Pengarang buku tidak boleh kosong',
-        }" />
-      <FormKit v-model="form.customerPhone" type="text" label="Email" validation="required"
-        validation-visibility="dirty" :validation-messages="{
-          required: 'Pengarang buku tidak boleh kosong',
-        }" />
-      <FormKit v-model="form.customerAddress" type="textarea" label="Address" rows="5" />
 
-      <!-- <rs-button> Submit </rs-button> -->
+      <FormKit v-model="form.ownerPassword" type="password" label="Password" validation="required"
+        validation-visibility="dirty" :validation-messages="{
+          required: 'Customer name cannot be empty',
+        }">
+      </FormKit>
+
+      <FormKit v-model="form.ownerUsername" type="text" label="Username" validation="required"
+        validation-visibility="dirty" :validation-messages="{
+          required: 'Username cannot be empty',
+        }">
+      </FormKit>
+
+      <FormKit v-model="form.ownerPhone" type="text" label="Phone Number" validation="required"
+        validation-visibility="dirty" :validation-messages="{
+          required: 'Phone Number cannot be empty',
+        }" />
+      <FormKit v-model="form.ownerEmail" type="email" label="Email" validation="required" validation-visibility="dirty"
+        :validation-messages="{
+          required: 'Email cannot be empty',
+        }" />
+      <FormKit v-model="form.ownerAddress" type="textarea" label="Address" rows="4" validation="required"
+        validation-visibility="dirty" :validation-messages="{
+          required: 'Address cannot be empty',
+        }" />
+
+      <div class="float-right">
+        <rs-button> Submit </rs-button>
+
+      </div>
+
     </FormKit>
+    <template #footer></template>
+
   </rs-modal>
 
   <div>
 
-    <div class="flex-1 bg-violet-500 col-span-1 ">
-      <div class="flex justify-between grid-cols-1  items-center bg-red-200">
+    <div class=" flex-1 bg-violet-500 col-span-1 ">
+      <div class=" flex justify-between grid-cols-1 items-center bg-red-200">
         <!-- image logo -->
-        <img src="/img/logo.png" alt="logo" class="w-20 h-20" />
+        <img src="@@/assets/img/logo/mewonity_logo_1.png" alt="logo" class="w-35 h-20" />
         <div class="flex grid-cols-1 gap-4 pr-8">
-          <div>Home</div>
-          <div>About Us</div>
-          <div @click="showModalTop = true">Register</div>
-          <div>Login</div>
+          <div>
+            <rs-button>
+              Home
+            </rs-button>
+          </div>
+          <div>
+            <rs-button>
+              About Us
+            </rs-button>
+          </div>
+          <div>
+            <rs-button @click="showModalTop = true">
+              Register
+            </rs-button>
+          </div>
+          <div>
+            <nuxt-link to="/login">
+              <rs-button>
+                Login
+              </rs-button>
+            </nuxt-link>
+          </div>
         </div>
 
       </div>
 
-      <div class="grid grid-flow-col justify-stretch pt-10 pb-10 h-80">
-        <div class="bg-warning"></div>
-        <div class="bg-red">You can make difference in their lives.</div>
+      <div class="grid grid-flow-col justify-stretch p-10 h-80">
+        <div class=" flex justify-end ">
+          <img src="@@/assets/img/meow_landingpage.png" alt="logo" class="h-70" />
+
+        </div>
+        <div class="flex bg-red-400">
+          <h1>
+            You can make difference in their lives.
+          </h1>
+        </div>
       </div>
     </div>
 
     <div class="flex justify-center grid-cols-3 p-8 ">
 
+      <div class="w-32 flex flex-col items-center">
+        <div class="h-10">
+          <h4>Today's Adoption</h4>
+        </div>
+        <span class="justify-center pt-6">8</span>
+      </div>
       <div class="w-32 flex flex-col items-center ">
-        <span>Today's Adoption</span>
-        <span class="justify-center">8</span>
+        <div class="h-10">
+          <h4>Total Pets</h4>
+        </div>
+        <span class="justify-center pt-6">10</span>
       </div>
 
       <div class="w-32 flex flex-col items-center ">
-        <span>Total Pets</span>
-        <span class="justify-center">10</span>
-      </div>
-
-      <div class="w-32 flex flex-col items-center ">
-        <span>Total Registered Owner</span>
-        <span class="justify-center">40</span>
+        <div class="h-10">
+          <h4>Total Registered Owner</h4>
+        </div>
+        <span class="justify-center pt-6">40</span>
 
       </div>
-    </div>
-    <div class="flex grid-cols-1">
-
-      <p>Find and adopt</p>
-      <rs-dropdown title="State">
-        <rs-dropdown-item> Option 1 </rs-dropdown-item>
-        <rs-dropdown-item> Option 2 </rs-dropdown-item>
-        <rs-dropdown-item> Option 3 </rs-dropdown-item>
-      </rs-dropdown>
 
     </div>
+    <div class="flex grid-cols-1 p-6 justify-center items-center">
 
-    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-x-6 ml-6">
+      <div class="w-40 ">
+        <p>Find and adopt </p>
+
+      </div>
+      <div class="w-40 ">
+        <rs-dropdown title="State" class="">
+          <rs-dropdown-item> Selangor</rs-dropdown-item>
+          <rs-dropdown-item> Kedah </rs-dropdown-item>
+          <rs-dropdown-item> Negeri Sembilan </rs-dropdown-item>
+        </rs-dropdown>
+      </div>
+
+
+    </div>
+
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-x-6 p-6">
       <rs-card v-for="(val, index) in petData" class="p-5 relative bg-black">
         <img :src="val.image" alt="image" class="w-full h-48 object-cover rounded-lg" />
-
-        <!-- <rs-badge
-          variant="warning"
-          class="absolute top-3 -right-3"
-        >
-          Featured
-        </rs-badge> -->
         <h5>
           {{ val.name }}
           <!-- test -->

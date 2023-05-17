153c1b64639c1ce6549c26e0eec01f83cb812f71
Faris37 <farisizwanfauzi@gmail.com>
2023-05-18 00:46:41 +0800

    APIKEY

---
+++ server/api/chat/getApi.get.js
@@ -0,0 +1,30 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+export default defineEventHandler(async (event) => {
+  //   const { username, password } = event.body;
+
+  const body = await readBody(event);
+
+  
+  // CHECK IF USER IS EXIST
+  const key = await prisma.lookup.findFirst({
+    where: {
+      lookupID: 1,
+    },
+  });
+
+  console.log(key)
+  
+  if (!key)
+    return {
+      statusCode: 400,
+      message: "key not found",
+    };
+
+  return {
+    statusCode: 200,
+    message: "Retrieve key detail",
+    data: key,
+  };
+});
